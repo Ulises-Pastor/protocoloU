ddfa9afa2d4cc37fc62f201be462652d14415b5e
Ulises-Pastor <pamu010914@gs.utm.mx>
2024-12-09 01:48:52 -0600

    Implementacion de reenvio de paquetes.

--- client/main_client.py
+++ client/main_client.py
@@ -5,41 +5,60 @@ import sys
 
 # Configuración de simulación
 mean_delay = 3  # Media para simulación del tráfico (segundos)
-std_dev_delay = 0.8  # Desviación estándar para simulación del tráfico (segundos)
+std_dev_delay = 0.5  # Desviación estándar para simulación del tráfico (segundos)
 windowC = 1.5 * mean_delay
+
+#0 el buffer esta lleno
+#1Hay espacio en el buffer
 
 def send_file(server_ip, file_name):
     try:
         global windowC
-        print(f"Ventana : {windowC}")
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((server_ip, 9999))  # Asume el puerto 9999 como predeterminado
-
-        # Medir tiempo real
-        start_time = time.time()
-
-        # Enviar archivo
-        with open(file_name, "rb") as f:
-            client.sendfile(f)
-
-        # Recibir respuesta del servidor
-        response = client.recv(1024).decode()
-        end_time = time.time()
-        response_time = end_time - start_time
-
-        # Simular retardo
-        simulated_delay = np.random.normal(mean_delay, std_dev_delay,1)[0]
-        total_time = response_time + simulated_delay
-        #print(f"Tiempo total: {total_time}")
-        if total_time < windowC:
-            print(f"Tiempo total (real + simulado): {total_time:.2f} segundos")
-            print(f"Respuesta del servidor: {response}")
-            windowC = response_time + 1.2*simulated_delay
-        else:
-            print(f"Tiempo de espera agotado.... Vuelva a enviar el paquete")
-            windowC = 1.5 * mean_delay
-
-        client.close()
+        enviado = False
+        intentos = 3
+        while(not enviado and intentos > 0):
+            print(f"Ventana : {windowC}")
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((server_ip, 9999))  # Asume el puerto 9999 como predeterminado
+            
+            #Respuesta inicial del servidor
+            initial_r = client.recv(1024).decode()
+            print(f"Respuesta inicial: {initial_r}")
+            accion = int(initial_r)
+            
+            #Evalua disponibilidad del buffer
+            if accion:
+                # Medir tiempo real
+                start_time = time.time()
+                # Enviar archivo
+                with open(file_name, "rb") as f:
+                    client.sendfile(f)
+                # Recibir respuesta del servidor
+                response = client.recv(1024).decode()
+                end_time = time.time()
+                response_time = end_time - start_time
+                # Simular retardo
+                simulated_delay = np.random.normal(mean_delay, std_dev_delay,1)[0]
+                total_time = response_time + simulated_delay
+                print(f"Tiempo total: {total_time:.4f}")
+                if total_time < windowC:
+                    print(f"Respuesta del servidor: {response}.")
+                    windowC = response_time + 1.1*simulated_delay
+                    enviado = True
+                else:
+                    print(f"Tiempo de espera agotado...")
+                    windowC = 1.5 * mean_delay
+                    print(f"Reenviando el paquete {file_name}...\n")
+            else:
+                #Espera un periodo de tiempo para intentar el reenvío
+                waiting_time = np.random.normal(mean_delay/2, std_dev_delay, 1)
+                print("Esperando para reenvio...")
+                time.sleep(int(waiting_time[0]))
+                print(f"Reenviando el paquete {file_name}...\n")
+                intentos -= 1
+            client.close()
+        if intentos <= 0:
+            print("(500) Internal Server Error :(")
     except FileNotFoundError:
         print(f"Error: El archivo '{file_name}' no existe.")
     except ConnectionError:
@@ -52,17 +71,15 @@ if __name__ == "__main__":
         print("Uso: python cliente.py <direccion_del_servidor>")
     else:
         server_ip = sys.argv[1]
-
         while True:
+            print("---------------------------------------------------------------")
             file_name = input("Ingrese el nombre del archivo a enviar (o 'salir' para terminar): ").strip()
-
             if file_name.lower() == "salir":
                 print("Terminando el programa.")
                 break
-
             send_file(server_ip, file_name)
-
             another = input("¿Desea enviar otro archivo? (s/n): ").strip().lower()
             if another != "s":
                 print("Terminando el programa.")
                 break
+            print("---------------------------------------------------------------\n")
--- server/main_server.py
+++ server/main_server.py
@@ -1,69 +1,57 @@
 import socket
 import threading
 import numpy as np
-import os
 import time
 
 # Configuración del buffer y simulación
 buffer_size = 4
-
 buffer_lock = threading.Lock()
-mean_time = 10  # Media para simulación del tiempo (segundos)
-std_dev_time = 2.5  # Desviación estándar para simulación del tiempo (segundos)
+mean_time = 20  # Media para simulación del tiempo
+std_dev_time = 2.5  # Desviación estándar para simulación del tiempo
 
 def handle_client(client_socket, addr):
     global buffer_size
-
     # Simulación de la recepción
     with buffer_lock:
+        if buffer_size < 1: #Espera un pequeño tiempo a que se libere un espacio
+            time.sleep(int(mean_time/4))
         if buffer_size > 0:
             buffer_size -= 1
+            client_socket.send(b"1")
+            print(f"Paquete de {addr} aceptado. ({buffer_size} disponibles)\n")
         else:
-            client_socket.send(b"Buffer lleno. Intente mas tarde.") #client_socket.send("Buffer lleno. Intente más tarde.".encode("utf-8"))
+            client_socket.send(b"0")
             client_socket.close()
+            print(f"Paquete de {addr} rechazado")
             return
-
     # Recibir archivo
     filename = f"archivo_recibido_{addr[1]}.jpg"
     with open(filename, "wb") as f:
             data = client_socket.recv(1024)
             if not data:
                 print("Error: No se recibió ningún dato.")
-                # Incrementar el buffer
+                # Incrementar el buffer cuando no se recibió información
                 with buffer_lock:
                     buffer_size += 1
                 return
             f.write(data)
-
     # Simular tiempo de "envío a la capa de aplicación"
     simulated_time = np.random.normal(mean_time, std_dev_time, 1)
-    
-    response = f"Conexión recibida. Buffer actual: {buffer_size}\n"
+    response = f"Conexión recibida. Buffer actual: {buffer_size}"
     client_socket.send(response.encode())
     client_socket.close()
-    """El servidor recibe el archivo y lo guarda en el sistema de archivos local. Luego, envía una respuesta al cliente con el estado del buffer y cierra la conexión. El tiempo de simulación se calcula aleatoriamente y se envía al cliente como parte de la respuesta."""
-    
-    """desde aqui se simula el tiempo del procesamiendo dl buffer  
-    |
-    |
-    V
-    """
+    #Inicia "envio a la capa de aplicación"
     time.sleep(int(simulated_time[0]))
-
     # Incrementar el buffer después del tiempo simulado
     with buffer_lock:
         buffer_size += 1
         print(f"Espacio del buffer liberado... ({buffer_size} disponibles)")
 
-    # Enviar respuesta al cliente
-    
-
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("0.0.0.0", 9999))
-    server.listen(5)
+    server.listen(5)    #Tamaño de cola de espera de solicitudes
     print("Servidor escuchando en el puerto 9999...")
-
     while True:
         client_socket, addr = server.accept()
         print(f"Conexión establecida con {addr}")
