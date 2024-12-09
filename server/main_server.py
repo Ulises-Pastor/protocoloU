447545f4f55acc259109ddb264fce7b0ab428f4e
Ulises-Pastor <pamu010914@gs.utm.mx>
2024-12-08 21:27:29 -0600

    Corrección en el servidor

--- server/main_server.py
+++ server/main_server.py
@@ -6,9 +6,10 @@ import time
 
 # Configuración del buffer y simulación
 buffer_size = 4
+
 buffer_lock = threading.Lock()
-mean_time = 2  # Media para simulación del tiempo (segundos)
-std_dev_time = 0.5  # Desviación estándar para simulación del tiempo (segundos)
+mean_time = 10  # Media para simulación del tiempo (segundos)
+std_dev_time = 2.5  # Desviación estándar para simulación del tiempo (segundos)
 
 def handle_client(client_socket, addr):
     global buffer_size
@@ -25,24 +26,38 @@ def handle_client(client_socket, addr):
     # Recibir archivo
     filename = f"archivo_recibido_{addr[1]}.jpg"
     with open(filename, "wb") as f:
-        while True:
             data = client_socket.recv(1024)
             if not data:
-                break
+                print("Error: No se recibió ningún dato.")
+                # Incrementar el buffer
+                with buffer_lock:
+                    buffer_size += 1
+                return
             f.write(data)
 
     # Simular tiempo de "envío a la capa de aplicación"
-    simulated_time = max(0, np.random.randn() * std_dev_time + mean_time)
-    time.sleep(simulated_time)
+    simulated_time = np.random.normal(mean_time, std_dev_time, 1) #se agrega un retardo de 40 segundos para sque se note mas el tiempo de procesamiento
+    #print(simulated_time)
+    
+    response = f"Conexión recibida. Buffer actual: {buffer_size}\n"
+    client_socket.send(response.encode())
+    client_socket.close()
+    """El servidor recibe el archivo y lo guarda en el sistema de archivos local. Luego, envía una respuesta al cliente con el estado del buffer y cierra la conexión. El tiempo de simulación se calcula aleatoriamente y se envía al cliente como parte de la respuesta."""
+    
+    """desde aqui se simula el tiempo del procesamiendo dl buffer  
+    |
+    |
+    V
+    """
+    time.sleep(int(simulated_time[0]))
 
     # Incrementar el buffer después del tiempo simulado
     with buffer_lock:
         buffer_size += 1
+        print(f"Espacio del buffer liberado... ({buffer_size} disponibles)")
 
     # Enviar respuesta al cliente
-    response = f"Conexión recibida. Buffer actual: {buffer_size}\n"
-    client_socket.send(response.encode())
-    client_socket.close()
+    
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
